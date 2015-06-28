479be22282c81f9773f1113abc6d858771ceed2e
Brian R. Bondy <netzen@gmail.com>
2015-06-28 19:03:38 -0400

    Remove explicit module id

--- dist/abp-filter-parser.js
+++ dist/abp-filter-parser.js
@@ -1,6 +1,6 @@
 (function (global, factory) {
   if (typeof define === 'function' && define.amd) {
-    define('abp-filter-parser', ['exports'], factory);
+    define(['exports'], factory);
   } else if (typeof exports !== 'undefined') {
     factory(exports);
   } else {
