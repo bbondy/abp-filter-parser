6d58f4366906e5d293a8441ce9e98f06907b6df8
Rohan <me@rohandey.com>
2016-06-29 02:15:11 +0530

    Update abp-filter-parser.js

--- src/abp-filter-parser.js
+++ src/abp-filter-parser.js
@@ -67,6 +67,36 @@ export function parseDomains(input, separator, options) {
     .map((domain) => domain.substring(1)));
 }
 
+if (!Array.prototype.includes) {
+  Array.prototype.includes = function(searchElement /*, fromIndex*/ ) {
+    'use strict';
+    var O = Object(this);
+    var len = parseInt(O.length, 10) || 0;
+    if (len === 0) {
+      return false;
+    }
+    var n = parseInt(arguments[1], 10) || 0;
+    var k;
+    if (n >= 0) {
+      k = n;
+    } else {
+      k = len + n;
+      if (k < 0) {k = 0;}
+    }
+    var currentElement;
+    while (k < len) {
+      currentElement = O[k];
+      if (searchElement === currentElement ||
+         (searchElement !== searchElement && currentElement !== currentElement)) { // NaN !== NaN
+        return true;
+      }
+      k++;
+    }
+    return false;
+  };
+}
+
+
 /**
  * Parses options from the passed in input string
  */
